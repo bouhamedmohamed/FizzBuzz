cda80c3b99cf6723047b9c22cbc517b5ac63bd5e
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-06 12:18:48 +0200

    should return fizzBuzz when we send 15

--- src/main/java/domain/FizzBuzz.java
+++ src/main/java/domain/FizzBuzz.java
@@ -4,10 +4,11 @@ public class FizzBuzz {
     public String calculateThePlay(String playToCalculate) {
         try {
             final int validPlayTocalculate = Integer.parseInt(playToCalculate);
-            if (validPlayTocalculate % 3 == 0)
+            if (validPlayTocalculate % 15 == 0)
+                return "fizzBuzz";
+            else if (validPlayTocalculate % 3 == 0)
                 return "fizz";
-            else
-            if (validPlayTocalculate % 5 == 0)
+            else if (validPlayTocalculate % 5 == 0)
                 return "buzz";
         } catch (NumberFormatException ex) {
         }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,35 +1,41 @@
 import domain.FizzBuzz;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class FizzBuzzTest {
     @Test
     public void should_return_empty_string_when_send_empty_string() {
 
         final FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("",fizzBuzz.calculateThePlay(""));
+        assertEquals("", fizzBuzz.calculateThePlay(""));
     }
+
     @Test
     public void should_return_1_when_send_1() {
 
         final FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("1",fizzBuzz.calculateThePlay("1"));
+        assertEquals("1", fizzBuzz.calculateThePlay("1"));
     }
 
     @Test
     public void should_return_fizz_when_send_3() {
 
         final FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("fizz",fizzBuzz.calculateThePlay("3"));
+        assertEquals("fizz", fizzBuzz.calculateThePlay("3"));
     }
 
     @Test
     public void should_return_buzz_when_send_5() {
 
         final FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("buzz",fizzBuzz.calculateThePlay("5"));
+        assertEquals("buzz", fizzBuzz.calculateThePlay("5"));
+    }
+
+    @Test
+    public void should_return_fizzBuzz_when_send_15() {
+
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("fizzBuzz", fizzBuzz.calculateThePlay("15"));
     }
 }
